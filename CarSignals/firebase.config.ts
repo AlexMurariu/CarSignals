b3b0aafe49442c65f117152723ef05dc7d3f4930
Alexandru Murariu <AlexandruMu@yonder.local>
2019-12-29 17:30:01 +0200

    Made the setup

---
+++ CarSignals/firebase.config.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBH1VLN7jMF1igwVIlH9X9n8XezQNRYI1I",
+    authDomain: "carsignals.firebaseapp.com",
+    databaseURL: "https://carsignals.firebaseio.com",
+    projectId: "carsignals",
+    storageBucket: "carsignals.appspot.com",
+    messagingSenderId: "904374988549",
+    appId: "1:904374988549:web:9cd54831de9db75e41916c"
+};
+
+let Firebase = firebase.initializeApp(firebaseConfig);
+
+export default Firebase;
